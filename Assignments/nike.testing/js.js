c6bca753c928984b3ef599a115c72f25e8eaffd4
aliwert <154356044+aliwert@users.noreply.github.com>
2024-03-18 19:58:40 +0300

    deleted

--- Assignments/nike.testing/js.js
+++
@@ -1,8 +0,0 @@
-function imgSlider(someThing) {
-    document.querySelector('.sports').src = someThing
-}
-
-function changeColor(color) {
-    const sec = document.querySelector('.home');
-    sec.style.background = color;
-}
