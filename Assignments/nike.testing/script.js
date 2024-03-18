5fed5f5b47412c4434f8bfc4912a5a95d35cc673
aliwert <154356044+aliwert@users.noreply.github.com>
2024-03-18 19:58:23 +0300

    updatedname

---
+++ Assignments/nike.testing/script.js
@@ -0,0 +1,8 @@
+function imgSlider(someThing) {
+    document.querySelector('.sports').src = someThing
+}
+
+function changeColor(color) {
+    const sec = document.querySelector('.home');
+    sec.style.background = color;
+}
